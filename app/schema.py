a4e26b27179dc1382a21ba36cd46eeb6b4107656
Francis <morets132@gmail.com>
2025-02-11 05:47:20 +0000

    7th commit

--- app/schema.py
+++ app/schema.py
@@ -1,6 +1,22 @@
 from ninja import Schema
 
 class WorkerSchema(Schema):
+    Employee_id : str
+    first_name : str
+    last_name : str
+    username : str
+
+class Products(Schema):
+    item_code : str
+    Part_no : str
+    process : str
+    customer : str
+    product_family : str
+
+class Worker_out(Schema):
+    lot_no : str
+    current_status : str
+    Out_data : str
 
 
 
